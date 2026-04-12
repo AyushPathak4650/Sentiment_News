b5c9e6198460bfbfbd11025ad6ccbbcf2720c187
AyushPathak4650 <ayushpathak4650@gmail.com>
2026-04-12 22:15:38 +0530

    Add WhiteNoise for static files and collectstatic

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Add WhiteNoise for static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
